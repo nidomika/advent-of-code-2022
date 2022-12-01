1a4726ba2e75a7f6e394eb4af1d7a50842eef6ee
Dominika Szymanska <dominika1872@gmail.com>
2022-12-02 00:43:55 +0100

    Complete day 1

--- 01/day1.js
+++ 01/day1.js
@@ -1,1 +1,20 @@
 import { readFileString, readFileInt } from '../functions.js'
+
+const input = readFileString('01/input.txt')
+const sums = []
+
+let curSum = 0
+
+input.forEach(line => {
+  if (line !== '') {
+    curSum += +line
+  } else {
+    sums.push(curSum)
+    curSum = 0
+  }
+})
+
+const sorted = sums.sort().reverse()
+
+console.log(sorted[0])
+console.log(sorted[0] + sorted[1] + sorted[2])
